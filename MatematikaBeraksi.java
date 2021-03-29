4b81b05523b58768207f6f8691229f4bdca8ce0b
assiva20 <80864647+assiva20@users.noreply.github.com>
2021-03-29 23:07:18 +0700

    Add files via upload

--- MatematikaBeraksi.java
+++ MatematikaBeraksi.java
@@ -4,14 +4,21 @@ NIM         : 11200930000029
 Kelas       : Sistem Informasi 2A
  */
 
-public class MatematikaBeraksi {
+package Matematika;
+public class MatematikaBeraksi extends Matematika {
 
     public static void main(String[] args) {
-        Matematika Beraksi = new Matematika();
+        Matematika mtk = new Matematika();
  
-        Beraksi.pertambahan(20,20);
-        Beraksi.pengurangan(10,5);
-        Beraksi.perkalian(10,20);
-        Beraksi.pembagian(21,2);
+        mtk.pertambahan(20,20);
+        mtk.pengurangan(10,5);
+        mtk.perkalian(10,20);
+        mtk.pembagian(21,2);
+        mtk.pertambahan(12.5,28.7,14.2);
+        mtk.pengurangan(12.5,28.7,14.2);
+        mtk.perkalian(12.5,28.7,14.2);
+        mtk.pembagian(12.5,28.7,14.2);
+        
+        
     }
 }
